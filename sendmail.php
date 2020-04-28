dfdfc90faccb2d8c04feffd3a13098df2f7feef4
Firoz Khan <firozsaifi1234@gmail.com>
2020-04-28 18:05:11 +0530

    add contact page

---
+++ sendmail.php
@@ -0,0 +1,42 @@
+<?php
+include("./class.phpmailer.php");
+
+if(isset($_POST['sendmail']))
+{
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+    $email_message = "Email Id : ".$email."<br/>"."Name: ".$name."<br/>"."Phone: ".$phone."<br/>"."Message: ".$message;
+    $subject="Quickbooks enquiry from website";
+    
+
+    $to = "official@qbcontact.com";
+    
+    $mail = new PHPMailer("true");
+    $mail->IsHTML(true);
+    
+    //$mail->Charset = "iso-8859-1";
+    $mail->CharSet = "UTF-8";
+    $mail->From = "official@qbcontact.com";
+    $mail->FromName = "official@qbcontact.com";
+    $mail->Subject = "Quickbooks enquiry from website";
+    $mail->Body = $email_message;
+    $mail->AddAddress("official@qbcontact.com");
+    
+    
+    if($mail->Send())
+    {
+       
+        ?>
+        <script>
+        window.location.href = "thank-you.html";
+        // alert("form submitted");
+        // header('Location: thank-you.html');
+        </script>
+    
+        <?php
+    }
+}
+?>
---
+++ thank-you.php
@@ -0,0 +1,101 @@
+<!Doctype html>
+<html lang="en">
+  <head>
+    <title>Thank You for Submitting form</title>
+    <meta name="description" content="Dial qb toll free number ✓ 100% Customer Resolution ✓ 100% Satisfaction ✓ 24X7 Customer Service Contact us +18002934209 call now !!">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="shortcut icon" type="image/png" href="images/favicon.png"/>
+
+    <style>
+        header {
+            background-color: #012f2f;
+            padding: 16px 0px;
+        }
+        header img {
+            width: 200px;
+        }
+        .thanks h1 {
+            text-transform: capitalize;
+            font-weight: 700;
+            margin-bottom: 20px;
+            font-size: 33px;
+        }
+        .thanks .btn-primary {
+            margin-top: 12px;
+            border-radius: 100px;
+            padding: 12px 23px;
+            font-weight: 700;
+        }
+        .thanks .fa-check-circle {
+            font-size: 100px;
+            display: inline-block;
+            margin-bottom: 20px;
+            color: green;
+        }
+
+        @media (min-width: 320px) and (max-width: 375px) {
+            header img {
+                width: 160px;
+            }
+            .thanks .fa-check-circle {
+                font-size: 80px;
+            }
+            .thanks h1 {
+                line-height: 24px;
+                font-size: 13px;
+            }
+        }
+        @media (min-width: 376px) and (max-width: 414px) {
+            header img {
+                width: 170px;
+            }
+            .thanks .fa-check-circle {
+                font-size: 90px;
+            }
+            .thanks h1 {
+                line-height: 28px;
+                font-size: 17px;
+            }
+        }
+        @media (min-width: 415px) and (max-width: 568px) {
+            header img {
+                width: 180px;
+            }
+            .thanks .fa-check-circle {
+                font-size: 90px;
+            }
+            .thanks h1 {
+                line-height: 30px;
+                font-size: 20px;
+            }
+        }
+    </style>
+
+  </head>
+
+  <body>
+
+    <header class="container-fluid text-center">
+        <a href="https://www.qbcontact.com">
+            <img src="images/logo-white.png" alt="" class="img-fluid">
+        </a>
+    </header>
+
+    <section class="container-fluid thanks">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 col-lg-12 col-sm-12 text-center py-5">
+                    <i class="fa fa-check-circle"></i>
+                    <h1>thank you for contacting us we will get <br>back to you soon</h1>
+                    <a href="https://www.qbcontact.com" class="btn btn-primary">Back To Home</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+  </body>
+</html>
+  
